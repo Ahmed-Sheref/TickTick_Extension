2e5fabad366e9dd70d361b21edb8d6bb666930fe
AhmeSheref <ahwy837@gmail.com>
2026-04-13 20:09:33 +0200

    update content routes and API structure

--- Routes/contentRoute.js
+++ Routes/contentRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as contentControl from '../Controllers/contentControl.js';
+import { asyncHandler } from '../middleware/errorMiddleware.js';
 
 const router = express.Router();
 
 router.route("/")
-    // .get(contentControl.getAllContents)
-    .post(contentControl.createContent);
+    // .get(asyncHandler(contentControl.getAllContents))
+    .post(asyncHandler(contentControl.createContent));
+
+router.get("/user/:userId", asyncHandler(contentControl.getContentByUserId));
 
 export default router;
