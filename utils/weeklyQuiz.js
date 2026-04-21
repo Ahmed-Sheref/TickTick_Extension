ec6d763f9a8166fb10364d66e96286c57751c31d
AhmeSheref <ahwy837@gmail.com>
2026-04-21 14:32:06 +0200

    Handle Time for emails and telegram

--- utils/weeklyQuiz.js
+++ utils/weeklyQuiz.js
@@ -6,7 +6,7 @@ import bot from './telegramBot.js';
 
 export const startWeeklyQuizCron = () =>
 {
-    cron.schedule('0 0 * * 0', async () =>
+    cron.schedule('*/20 * * * * *', async () =>
     {
         console.log('[QUIZ] Starting weekly quiz job...');
 
